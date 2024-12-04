75fc722aeea59f3882ac1cd45522c60f332e32f7
Pudding <gkscksgma14@gmail.com>
2024-12-04 16:38:02 +0900

    feat: Add Cat Scriptable Object
    1. Cat정보를 Scriptable Object로 저장

---
+++ Cat_Merge/Assets/1.Scripts/Cat.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CatData", menuName = "ScriptableObjects/Cat", order = 1)]
+public class Cat : ScriptableObject
+{
+    // 고양이 고유 번호
+    [SerializeField] private int catId;
+    public int CatId { get => catId; set => catId = value; }
+
+    // 고양이 이름
+    [SerializeField] private string catName;
+    public string CatName { get => catName; set => catName = value; }
+
+    // 고양이 전투력 (임시로 catID * 50)
+    [SerializeField] private int catDamage { get => catId * 50; }
+    public int CatDamage { get => catDamage; }
+
+    // 고양이 이미지
+    [SerializeField] private Sprite catImage;
+    public Sprite CatImage { get => catImage; set => catImage = value; }
+    
+}

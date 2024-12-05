15d3c30ed9ff9a49945720c51427007bfe6e5382
Pudding <gkscksgma14@gmail.com>
2024-12-05 16:24:09 +0900

    refactor: CatData Script
    1. 코드 정리

--- Cat_Merge/Assets/1.Scripts/Merge Sysytem/CatData.cs
+++ Cat_Merge/Assets/1.Scripts/Merge Sysytem/CatData.cs
@@ -5,7 +5,7 @@ public class CatData : MonoBehaviour
 {
     private Cat catData;                    // 고양이 데이터
     private Image catImage;                 // 고양이 이미지
-    private CatDragAndDrop catDragAndDrop;  // 
+    private CatDragAndDrop catDragAndDrop;  // CatDragAndDrop 참조
 
     private void Awake()
     {
@@ -27,11 +27,13 @@ public class CatData : MonoBehaviour
         catImage.sprite = catData.CatImage;
     }
 
-    // Cat 데이터 설정 (GameObject가 활성화될 때 외부에서 호출될 수 있음)
+    // Cat 데이터 설정
     public void SetCatData(Cat cat)
     {
         catData = cat;
 
         UpdateCatUI();
     }
+
+
 }

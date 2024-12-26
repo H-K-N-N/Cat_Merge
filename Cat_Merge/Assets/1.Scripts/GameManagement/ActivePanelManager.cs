d3811233cff3c6382d3f944b174342022d829ec4
Pudding <gkscksgma14@gmail.com>
2024-12-26 18:27:01 +0900

    feat: ActivePanelManager (Panel 하나만 활성화) 제작
    1. 도감이 활성화된 상태에서 퀘스트를 누르면 이전에 활성화된 Panel 비활성화하는 코드 작성
    2. 재사용성을 챙긴 코드여서 다른 버튼에도 적용 가능

---
+++ Cat_Merge/Assets/1.Scripts/GameManagement/ActivePanelManager.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivePanelManager : MonoBehaviour
+{
+    private class PanelInfo
+    {
+        public GameObject Panel { get; }
+        public Image ButtonImage { get; }
+
+        public PanelInfo(GameObject panel, Image buttonImage)
+        {
+            Panel = panel;
+            ButtonImage = buttonImage;
+        }
+    }
+
+    private Dictionary<string, PanelInfo> panels = new Dictionary<string, PanelInfo>();     // Panel과 버튼 정보를 저장할 Dictionary
+    private string activePanelName = null;
+
+    // Panel과 버튼 등록
+    public void RegisterPanel(string panelName, GameObject panel, Image buttonImage)
+    {
+        if (!panels.ContainsKey(panelName))
+        {
+            panels.Add(panelName, new PanelInfo(panel, buttonImage));
+        }
+    }
+
+    // Panel 여닫는 함수
+    public void TogglePanel(string panelName)
+    {
+        if (panels.ContainsKey(panelName))
+        {
+            // 열려있던 Panel 버튼을 한번 더 눌렀다면
+            if (activePanelName == panelName)
+            {
+                ClosePanel(activePanelName);
+                activePanelName = null;
+            }
+            else
+            {
+                // 현재 열려 있는 Panel 닫기
+                if (activePanelName != null && activePanelName != panelName && panels.ContainsKey(activePanelName))
+                {
+                    ClosePanel(activePanelName);
+                }
+
+                // 새로운 Panel 열기
+                PanelInfo panelInfo = panels[panelName];
+                panelInfo.Panel.SetActive(true);
+                UpdateButtonColor(panelInfo.ButtonImage, true);
+
+                activePanelName = panelName;
+            }
+        }
+    }
+
+    // Panel 닫는 함수
+    public void ClosePanel(string panelName)
+    {
+        if (panels.ContainsKey(panelName))
+        {
+            PanelInfo panelInfo = panels[panelName];
+            panelInfo.Panel.SetActive(false);
+            UpdateButtonColor(panelInfo.ButtonImage, false);
+        }
+    }
+
+    // 버튼 색상 업데이트
+    private void UpdateButtonColor(Image buttonImage, bool isActive)
+    {
+        string colorCode = isActive ? "#5f5f5f" : "#FFFFFF";
+        if (ColorUtility.TryParseHtmlString(colorCode, out Color color))
+        {
+            buttonImage.color = color;
+        }
+    }
+
+}
